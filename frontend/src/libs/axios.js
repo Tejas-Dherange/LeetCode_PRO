03dd248512f68519fda50d60e6c8b5ba5a9624f2
Tejas Dherange <tejasdherange0099@gmail.com>
2025-06-07 16:49:56 +0530

    api urls

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,9 +1,7 @@
-import axios from "axios";
-
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api/v1"
-      : "/api/v1",
-      withCredentials: true,
+      ? "http://localhost:3000/api/v1" // For local dev
+      : "https://api.codeloom.software/api/v1", // For production
+  withCredentials: true,
 });
