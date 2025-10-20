5ed3921b90459f0830a3fbbb7b72eb9e3e6ddf2d
Tejas Dherange <tejasdherange0099@gmail.com>
2025-10-20 12:28:33 +0530

    Resolved merge conflicts

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://www.codeloom.software",
+    origin: ["http://localhost:5173","*"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
